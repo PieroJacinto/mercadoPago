22f50375d28e6352ddd0571ab1ad78a4beb950cd
Piero <pierojacinto@gmail.com>
2023-05-10 15:27:19 -0300

    item added

---
+++ controllers/main.controller.js
@@ -0,0 +1,39 @@
+const {index,one} = require('../models/product.model');
+
+module.exports ={
+    index: (req,res) => res.render('home',{
+        products:index(),
+        styles:['home']
+    }),
+   
+    addCart: (req,res) => {
+        // Find Product in DB        
+        let product = one(req.body.id);
+        // Check product exist in cart
+        if (req.session.cart.find(item => item.id == product.id)){
+            // Case 1: Exist and update quantity
+            req.session.cart = req.session.cart.map(item => {
+                if (item.id == product.id) {
+                    item.quantity = item.quantity + 1
+                }
+                return item
+            })
+        }else {
+            // Case 2: Add cart and set quantity
+            req.session.cart.push({...product, quantity:1})
+        }
+        return res.redirect("/")
+    },            
+   
+    // Step 5
+    updateCart: (req,res) => {
+        // Check quantity
+        // Case 1: Is equal to zero then remove product
+        // Case 2: Update all cart items setting quantity in product selected
+        return res.send("Update quantity")
+    }, 
+    // Step 6
+    removeCart: (req,res) =>{
+        return res.send("Remove a product from the cart")
+    }
+}
